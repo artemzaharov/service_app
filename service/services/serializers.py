2b850458cf851ab0c0eb7c9d1229a037cded0f35
Your Name <you@example.com>
2023-02-28 15:18:52 +0100

    3.25(2)

--- service/services/serializers.py
+++ service/services/serializers.py
@@ -1,11 +1,12 @@
 from rest_framework import serializers
 from .models import Subscription
 
+
 class SubscriptionsSerializer(serializers.ModelSerializer):
-    client_name = serializers.CharField(source='client.company_name')
+    client_name = serializers.CharField(source="client.company_name")
     # source='client.user.email' used to get the email from the user model
-    email = serializers.CharField(source='client.user.email')
+    email = serializers.CharField(source="client.user.email")
 
     class Meta:
         model = Subscription
-        fields = ('id', 'plan_id', 'client_name', 'email')
+        fields = ("id", "plan_id", "client_name", "email")
